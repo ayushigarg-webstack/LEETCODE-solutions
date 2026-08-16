7ab08dd84de4457d633cadb0e50eccac40668b4d
Ayushi <ayushi.ac.2006@gmail.com>
2026-08-16 21:39:29 +0530

    Time: 1 ms (82.74%), Space: 131.1 MB (92.89%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+         int cnt[3] = {0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        // Even number of 0-remainder stones
+        if (cnt[0] % 2 == 0) {
+            // We need at least one 1 and one 2
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
+
+        // Odd number of 0-remainder stones
+        return abs(cnt[1] - cnt[2]) >= 3;
+    }
+};
